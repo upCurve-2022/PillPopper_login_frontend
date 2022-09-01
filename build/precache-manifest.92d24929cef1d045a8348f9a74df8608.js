b19d4e0c5eeb4cb68dcf5de4fe3016ffae6ab1fc
Jaspriya Gujral <jaspriya12@gmail.com>
2022-09-01 21:42:55 +0530

    Add files via upload

---
+++ build/precache-manifest.92d24929cef1d045a8348f9a74df8608.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "229eaf84e72e4fffdc5c67e8dc393a5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "214daff4da123e56a7f5",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "5007d4b77ece2ddaed16",
+    "url": "/static/css/main.e068e46c.chunk.css"
+  },
+  {
+    "revision": "214daff4da123e56a7f5",
+    "url": "/static/js/2.09037476.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.09037476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5007d4b77ece2ddaed16",
+    "url": "/static/js/main.05ed6035.chunk.js"
+  },
+  {
+    "revision": "44aee220bccf4ace3598",
+    "url": "/static/js/runtime-main.d4e61eff.js"
+  }
+]);
